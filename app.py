2b911ebb392c8e30a0d412045c5ee2dc32809c6c
Bhakta Bhusan Das <bhaktabhaktabhakta@gmail.com>
2025-06-12 16:40:21 +0530

    Updating the version

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, DevOps!"
+    text = "Hello DevOps!\nVersion: 2"
+    return Response(text, mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
